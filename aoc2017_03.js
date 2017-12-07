f7ca4be916a07d16244a9ae96fb9ab89ee20a68c
frostySeasoning <34304990+frostySeasoning@users.noreply.github.com>
2017-12-07 11:42:24 +0100

    solution for task 2

--- aoc2017_03.js
+++ aoc2017_03.js
@@ -1,3 +1,5 @@
+// position/value = 277678
+
 function calcManhattenDistance(position)
 {
   if(position < 1)
@@ -19,4 +21,70 @@ function calcManhattenDistance(position)
   var pR = position - (k-2)*(k-2);
   
   return xR + Math.abs((xRC / 8) - pR % (xRC / 4));
+}
+
+function stresstest(value)
+{
+  return stresstestRek(2, 5, [1, 2, 4, 5, 10, 11, 23, 25], value)
+}
+
+function stresstestRek(x, xA, ringPreX, value)
+{
+  // x: x-ter Ring
+  // xA: Kantenlaenge des Rings
+  // Anzahl der Elemente im x-ten Ring
+  var xC = (xA * xA) - ((xA-2) * (xA-2));
+  var xC4 = xC/4;
+  var preXC = ringPreX.length;
+  // x-ten Ring mit 0 initialisieren
+  var ringX = Array.apply(null, Array(xC)).map(e => 0);
+  
+  // x-ten Ring berechnen
+  // Laufvar: Position auf dem x-ten Ring
+  for(var pX = 0; pX < xC; pX++)
+  {
+    // Positions-Korrektur (adjustment) bzgl des (x-1)-ten Rings
+    pAdj = -2 * Math.floor(pX/xC4);
+    if(pX % xC4 == 0)
+    {
+      ringX[pX] = ringPreX[pX+pAdj] + ringPreX[getPositionInCircularArray(pX+pAdj-1, preXC)] + ringX[getPositionInCircularArray(pX-1, xC)] + ringX[getPositionInCircularArray(pX-2, xC)];
+    }
+    else if(pX % xC4 == xC4-2)
+    {
+      ringX[pX] = ringX[getPositionInCircularArray(pX+2, xC)] + ringPreX[pX+pAdj-1] + ringPreX[pX+pAdj-2] + ringX[pX-1];
+    }
+    else if(pX % xC4 == xC4-1)
+    {
+      ringX[pX] = ringX[getPositionInCircularArray(pX+1, xC)] + ringPreX[pX+pAdj-2] + ringX[pX-1];
+    }
+    else
+    {
+      ringX[pX] = ringPreX[pX+pAdj] + ringPreX[pX+pAdj-1] + ringPreX[getPositionInCircularArray(pX+pAdj-2, preXC)] + ringX[pX-1];
+    }
+    console.log(ringX[pX]);
+    // Austrittsbedingung
+    if(ringX[pX] > value)
+    {
+      return ringX[pX];
+    }
+  }
+  return stresstestRek(x+1, xA+2, ringX, value);
+}
+
+function getPositionInCircularArray(p,l)
+{
+  // p: position
+  // l: array length
+  while(p < 0 || p >= l)
+  {
+    if(p < 0)
+    {
+      p += l;
+    }
+    if(p >= l)
+    {
+      p -= l;
+    }
+  }
+  return p;
 }
